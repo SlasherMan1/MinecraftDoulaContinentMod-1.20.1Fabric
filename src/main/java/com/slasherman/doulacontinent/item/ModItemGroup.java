0f393d75d32c693e716eaaaaa1ac30df2041b71b
SlasherMan <3448241291@qq.com>
2025-02-06 13:38:04 +0800

    修改为Register<ItemGroup>类型

--- src/main/java/com/slasherman/doulacontinent/item/ModItemGroup.java
+++ src/main/java/com/slasherman/doulacontinent/item/ModItemGroup.java
@@ -9,25 +9,38 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 
 public class ModItemGroup {
-    public static final ItemGroup DOULACONTINENT_GROUP = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(DoulaContinentMod.MOD_ID, "doulacontinent_group"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemGroup.doulacontinent_group"))
-                    .icon(()-> new ItemStack(ModItems.WanNianHunHuan)).entries((displayContext, entries) ->
+
+    public static final RegistryKey<ItemGroup> DOULACONTINENT_GROUP = register("doulacontinent_group");
+
+    private static RegistryKey<ItemGroup> register(String id) {
+        return RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(DoulaContinentMod.MOD_ID, id));
+    }
+
+    public static final ItemGroup registerGroup = Registry.register(
+            Registries.ITEM_GROUP,
+            DOULACONTINENT_GROUP,
+            ItemGroup.create(ItemGroup.Row.TOP,8)
+                    .displayName(Text.translatable("itemGroup.doulacontinent_group"))
+                    .icon(()-> new ItemStack(ModItems.WanNianHunHuan))
+                    .entries((displayContext, entries) ->
                     {
-                       entries.add(ModItems.WanNianHunHuan);
-                       entries.add(ModItems.ShiNianHunHuan);
-                       entries.add(Items.BOOK);
-                       entries.add(Blocks.ANVIL);
-                       entries.add(ModBlocks.HaiShenJinKuai);
-                       entries.add(ModItems.HaiShenJinDing);
-                       entries.add(ModBlocks.HaiShenJinOre);
-                       entries.add(ModItems.PROSPECTOR);
-                       entries.add(ModItems.HUNJI1_PHOENIX_FIRE);
+                        entries.add(ModItems.WanNianHunHuan);
+                        entries.add(ModItems.ShiNianHunHuan);
+                        entries.add(Items.BOOK);
+                        entries.add(Blocks.ANVIL);
+                        entries.add(ModBlocks.HaiShenJinKuai);
+                        entries.add(ModItems.HaiShenJinDing);
+                        entries.add(ModBlocks.HaiShenJinOre);
+                        entries.add(ModItems.PROSPECTOR);
+                        entries.add(ModItems.HUNJI1_PHOENIX_FIRE);
+
 
                     }).build());
     public static void registerModItemGroup(){
